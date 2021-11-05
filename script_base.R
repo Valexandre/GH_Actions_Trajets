16244120f73cd5723ad14574d4755967b2f2b318
Valexandre <Valexandre@users.noreply.github.com>
2021-11-05 17:13:03 +0100

    Update script_base.R

--- script_base.R
+++ script_base.R
@@ -11,12 +11,13 @@ library(rtweet)
 #  access_secret =   Sys.getenv("TWITTER_TOKEN_SECRET")
 #)
 
-rtweet::auth_as(rtweet::rtweet_bot(
+rtweet::rtweet_bot(
   api_key = Sys.getenv("TWITTER_KEY"),
   api_secret = Sys.getenv("TWITTER_SECRET_KEY"),
   access_token =  Sys.getenv("TWITTER_TOKEN"),
   access_secret =  Sys.getenv("TWITTER_TOKEN_SECRET")
- ))
+ )
+
 # Example: post a tweet via the API
 # The keys will are in your environment thanks to create_token()
 rtweet::post_tweet(status = "This is a test tweet.")
