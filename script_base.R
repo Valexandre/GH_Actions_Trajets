bbc943d213aac6055e48fbd81b2c2cf70f54087e
Valexandre <Valexandre@users.noreply.github.com>
2021-11-08 21:37:07 +0100

    ajout suite

--- script_base.R
+++ script_base.R
@@ -107,8 +107,6 @@ OuVaTravaillerLaCommune<-MOBPRO18_S%>%filter(CODGEORES==ComSelec)%>%
 #Poste Premier Tweet
     rtweet::post_tweet(status = paste0(Phrase$TextePart1," ",CreationHashTag))
     print("ok premier tweet")
-    ###########
-    #Et le second
       
 UnionsCommunesDep<-Inter_V_Donnees%>%st_transform(crs=2154)%>%
       group_by(substr(code,1,2))%>%
@@ -145,9 +143,104 @@ agg_png(paste0("data/CarteActifs",nomcomm$nom[1],".jpg"), width=900, height = 90
 Carte
 invisible(dev.off())
 
+
+DOuVientTravailler<-MOBPRO18_S%>%filter(DCLT==ComSelec)%>%
+  group_by(CODGEORES)%>%
+  summarise(Act=round(sum(IPONDI),0))%>%
+  ungroup()%>%
+  mutate(Part=round(Act/sum(Act)*100,2))
+
+
+
+GraphDOuVientTravailler<-DOuVientTravailler%>%top_n(.,n=3,wt = Part)%>%
+  left_join(Varmod_MOBPRO_2018%>%filter(COD_VAR=="COMMUNE")%>%select(COD_MOD,LIB_MOD),by=c("CODGEORES"= "COD_MOD"))%>%
+  mutate(LIB_MOD=case_when(is.na(LIB_MOD) & substr(CODGEORES,1,2)==75~paste0("Paris ",substr(CODGEORES,4,5)),
+                           is.na(LIB_MOD) & substr(CODGEORES,1,2)==69~paste0("Lyon ",substr(CODGEORES,4,5)),
+                           is.na(LIB_MOD) & substr(CODGEORES,1,2)==13~paste0("Marseille ",substr(CODGEORES,4,5)),
+                           TRUE~LIB_MOD))%>%arrange(desc(Part))
+
+PhATL2<-PhAT%>%filter(categorie=="douvient_podium")%>%
+  slice_sample(.,n=1)%>%
+  select(part1,part2)
+
+
+Phrase2<-PhATL2%>%mutate(TextePart1=str_replace(str_replace(str_replace(str_replace(
+  part1,"VILLE1",paste0(GraphDOuVientTravailler$LIB_MOD[1]," ",Virg(GraphDOuVientTravailler$Part[1]),"%")),
+  "VILLE2",paste0(GraphDOuVientTravailler$LIB_MOD[2]," ",Virg(GraphDOuVientTravailler$Part[2]),"%")),
+  "VILLE3",paste0(GraphDOuVientTravailler$LIB_MOD[3]," ",Virg(GraphDOuVientTravailler$Part[3]),"%")),
+  "A_LA_VILLE",nomcomm$ALaVille[1]))
+
+
+reply_id <- rtweet::get_timeline("humeursdevictor",n=1)$id_str
+
+rtweet::post_tweet(status=Phrase2$TextePart1[1],
+                   in_reply_to_status_id = reply_id)
+
+Inter_V_DonneesDOuVient<-Inter_V%>%left_join(DOuVientTravailler,by=c("code"="CODGEORES"))
+Inter_V_DonneesDOuVient<-Inter_V_DonneesDOuVient%>%mutate(Part=case_when(is.na(Part)~0,
+                                                                         TRUE~Part))
+Inter_V_DonneesDOuVient<-Inter_V_DonneesDOuVient%>%mutate(PartD=case_when(Part==0 ~"0%",
+                                                                          Part>0 & Part <= 1~"Entre 0 et 1%",
+                                                                          Part>1 & Part <= 3~"Entre 1 et 3%",
+                                                                          Part>3 & Part <= 5~"Entre 3 et 5%",
+                                                                          Part>5 & Part <= 15~"Entre 5 et 15%",
+                                                                          Part>15~"Plus de 15%"))
+UnionsCommunesDep<-Inter_V_DonneesDOuVient%>%st_transform(crs=2154)%>%
+  group_by(substr(code,1,2))%>%
+  summarise()%>%ungroup()%>%rename(code=1)
+
+couleursdouvient<-c("0%"="#FFFFFF","Entre 0 et 1%"="#CFDDE5","Entre 1 et 3%"="#A0BBCC",
+                    "Entre 3 et 5%"="#7199B2","Entre 5 et 15%"="#427799","Plus de 15%"="#135680")
+nomcomm<-CarteComm%>%filter(code==ComSelec)%>%st_drop_geometry()%>%left_join(TouslesLibellesDesVilles,by=c("code"="com"))
+
+Carte2<-Inter_V_DonneesDOuVient%>%st_transform(crs=2154)%>%ggplot()+
+  geom_sf(aes(fill=PartD,colour=code==ComSelec))+
+  geom_sf(data=Zone20Bornes%>%st_transform(crs=2154),colour="#135680",fill=NA,linetype="dashed")+
+  geom_sf_text(data=CarteComm%>%filter(code%in%JusteCommunesImportantes$INSEE_COM)%>%
+                 filter(code%in%Inter_V_DonneesDOuVient$code)%>%
+                 group_by(substr(code,1,2))%>%
+                 slice_sample(.,n=2),
+               aes(label=nom),check_overlap = T,size=3,
+               family = "Calibri")+theme_void()+
+  geom_sf(data=UnionsCommunesDep,fill=NA,colour="#22222280")+
+  geom_sf_text(data=UnionsCommunesDep%>%filter(code!=75),aes(label=code),colour="#22222260",size=6,alpha=0.8,family="Calibri")+
+  scale_fill_manual("",values=couleursdouvient)+
+  scale_colour_manual("",values=c("#22222230","#222222"))+
+  theme(legend.position="top",
+        plot.title =element_text(hjust=0,family = "Garamond",size=14),
+        plot.title.position =  "plot",
+        plot.caption.position =   "plot",
+        plot.subtitle = element_text(hjust=0,family = "Garamond",size=10),
+        plot.caption = element_text(hjust=1,family = "Garamond",size=8),
+        text=element_text(size=14,family = "Garamond"),
+        legend.text = element_text(size=10),
+        plot.margin = margin(0,0,0,0))+
+  guides(fill=guide_legend(nrow=2,byrow=TRUE),colour=F)+
+  labs(title=paste0("D'où viennent les actifs qui travaillent ",nomcomm$ALaVille[1],"?"),
+       subtitle="Part des actifs se rendant au travail dans la commune depuis moins de 20 km",
+       caption = "Données Insee, traitement Victor Alexandre @humeursdevictor")
+
+agg_png(paste0("data/CarteActifsProvenance",nomcomm$nom[1],".jpg"), width=900, height = 900, res = 144)
+Carte2
+invisible(dev.off())
+
+
 Sys.sleep(3)
-reply_id <- rtweet::get_timeline("humeursdevictor",n=1)$id
+reply_id <- rtweet::get_timeline("humeursdevictor",n=1)$id_str
 
 rtweet::post_tweet(status=PhATL$part2[1],
-               in_reply_to_status_id = reply_id,media = paste0("data/CarteActifs",nomcomm$nom[1],".jpg"))
+               in_reply_to_status_id = reply_id,media = c(paste0("data/CarteActifs",nomcomm$nom[1],".jpg"),
+                                                          paste0("data/CarteActifsProvenance",nomcomm$nom[1],".jpg")))
  print("ok 2eme tweet")
+
+ 
+#On regarde le csv des données si quelque chose a déjà été twitté à propos de cette commune pour le rajouter en suite.
+dejaparus <- read_csv("data/dejaparus.csv", col_types = cols(codeinsee = col_character(), datetweets = col_character()))
+dejaparus_communes<-dejaparus%>%filter(codeinsee==ComSelec)%>%summarise(TweetsPublies=paste0(lientweet,collapse = ", "))
+
+Sys.sleep(3)
+reply_id <- rtweet::get_timeline("humeursdevictor",n=1)$id_str
+
+if(nchar(dejaparus_communes$TweetsPublies)>4){
+  rtweet::post_tweet(status=paste0("Pour rappel, Nous avions déjà parlé ",nomcomm$DeLaVille, " ici : ",dejaparus_communes$TweetsPublies),in_reply_to_status_id = reply_id)
+}
